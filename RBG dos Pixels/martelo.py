423f0e31b94b75e3f80149ba00ee429688aacc07
Tristan Jeferson <jefersonalmeeida2@gmail.com>
2024-07-05 10:45:02 -0300

    Update martelo.py

--- RBG dos Pixels/martelo.py
+++ RBG dos Pixels/martelo.py
@@ -1,32 +1,32 @@
 from PIL import Image
 
-# Abrir a imagem
+# Abre a imagem
 imagem = Image.open("imagens/martelo pixel.jpg")
 
-# Redimensionar a imagem para 20x20, se necessário
+# Redimensiona a imagem para 20x20, se necessário
 imagem = imagem.resize((20, 20))
 
-# Pegar a largura e a altura da imagem
+# Pega a largura e a altura da imagem
 largura, altura = imagem.size
 
-# Criar uma matriz para armazenar os valores RGB
+# Cria uma lista para armazenar os valores RGB divididos por 32 em formato de uma matriz
 matriz_rgb = []
 
 for y in range(altura):
-    linha = []
     for x in range(largura):
-        # Usar getpixel para obter o valor RGB do pixel
+        # Usa getpixel para obter o valor RGB do pixel
         cor = imagem.getpixel((x, y))
 
-        # Dividir os valores RGB por 32
+        # Divide os valores RGB por 32
         r, g, b = cor
         r_dividido = r // 32
         g_dividido = g // 32
         b_dividido = b // 32
-        linha.append((r_dividido, g_dividido, b_dividido))
-    matriz_rgb.append(linha)
 
+        # Adiciona a cor dividida à lista
+        matriz_rgb.append((r_dividido, g_dividido, b_dividido))
 
-# Imprimir a matriz de valores RGB
-for linha in matriz_rgb:
-    print(linha)
+# Imprime a matriz de valores RGB divididos por 32
+for cor in matriz_rgb:
+    print(cor)
+
