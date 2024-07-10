4eff59207067cb57f3e7c2ead4309666320ec1c1
Leticia Ribeiro <68934064+leticiaribeiro7@users.noreply.github.com>
2024-07-10 17:30:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main() {
     int fd;
     int x=0, y=0;
     int leftButton;
-    int x_disp, y_disp;
+    signed char x_disp, y_disp;
     char mouse_buffer[3];
 
 
@@ -33,27 +33,59 @@ int main() {
         perror("Não é possível abrir o dispositivo do mouse");
         exit(EXIT_FAILURE);
     }
+    int i;
+
+    clear_sprite();
 
     //grava martelo
-   for (int i = 0; i < 400; i++) {
-        write_sprite_mem(martelo[i][0], martelo[i][1], martelo[i][2], i);
+    int j;
+    j = 0;
+    for (i = 10000; i < 10400; i++) {
+        int R = martelo[j][0];
+        int G = martelo[j][1];
+        int B = martelo[j][2];
+        j++;
+        write_sprite_mem(R, G, B, i);
     }
 
     Sprite_Fixed martelo;
     martelo.coord_x = 0;
     martelo.coord_y = 0;
-    martelo.offset = 0;
+    martelo.offset = 25;
     martelo.ativo = 1;
     martelo.data_register = 1;
 
-    set_sprite(martelo.data_register, martelo.coord_x, martelo.coord_y, martelo.offset, martelo.ativo);
+    Sprite sp_movel;
+    sp_movel.coord_x = 100;
+    sp_movel.coord_y = 150;
+    sp_movel.offset = 9;
+    sp_movel.data_register = 2;
+    sp_movel.ativo = 1;
+    sp_movel.collision = 0;
+
+    //set_sprite(martelo.data_register, martelo.coord_x, martelo.coord_y, martelo.offset, martelo.ativo);
+            int p;
+            int q;
+            p = 0; q = 0;
+
+     while(1) {
+
+        set_sprite(2, p, q, 3, 1);
+
+        p++;
+        q++;
+
+        usleep(50000);
+
+        }
     
     while(1) {
+
         if (read(fd, &mouse_buffer, sizeof(mouse_buffer)) > 0 ) { 
             system("clear");
             //define_poligon(1, 7, 1, 1, 0, x, y, 1); // limpa poligono
-            //clear_sprite();
-            set_sprite(martelo.data_register, x, y, martelo.offset, 0);
+
+            //set_sprite(martelo.data_register, x, y, martelo.offset, 0);
 
             x_disp = mouse_buffer[1];
             y_disp = mouse_buffer[2];
@@ -62,11 +94,19 @@ int main() {
             x += x_disp;
             y -= y_disp;
 
-            set_sprite(martelo.data_register, x, y, martelo.offset, martelo.ativo);
+            set_sprite(martelo.data_register, x, y, martelo.offset, 1);
+
             limitarCursor(&x, &y);
+
+
+            if (collision(&sp_movel, &martelo)) {
+                printf("Colisão detectada!\n");
+                // Lógica adicional para tratar a colisão
+            }
+
             printf("Posição X: %d, Posição Y: %d\n", x, y);
 
-            usleep(50000); // Aguarda por 50ms antes de verificar novamente (ajuste conforme necessário)
+            //usleep(50000); // Aguarda por 50ms antes de verificar novamente (ajuste conforme necessário)
             }        
         }
     }
