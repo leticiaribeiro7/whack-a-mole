09092cf98ba97c496ddf8889c85cc40eb8fa52c2
Tristan Jeferson <jefersonalmeeida2@gmail.com>
2024-08-01 13:57:42 -0300

    adicionado comentários

--- main.c
+++ main.c
@@ -148,12 +148,18 @@ void* movimentoToupeira(void* arg) {
     while (1) {
 
         readButtons();
+
         /**
          * \brief Inicia o jogo se o botão de iniciar for pressionado e o estado for START
          * 
          * Quando o botão de iniciar (button0) é pressionado e o estado atual é START, 
          * a função limpa o bloco de fundo, desenha a tela do jogo e define o estado para RUNNING.
          * O tempo de início e o último tempo de verificação são registrados.
+         * \param[in]       button0: Estado do botão de iniciar
+         * \param[in]       state: Estado atual do jogo
+         * \param[out]      state: Novo estado do jogo após a ação
+         * \param[out]      start_time: Tempo de início do jogo
+         * \param[out]      last_check_time: Último tempo de verificação do jogo
          */
         if (button0 && state == START) {
             clear_background_block();
@@ -168,6 +174,10 @@ void* movimentoToupeira(void* arg) {
          * 
          * Quando o botão de pausar (button1) é pressionado e o estado atual é RUNNING, 
          * a função define o estado para PAUSED, desenha os blocos de pausa e registra o tempo de pausa.
+         * \param[in]       button1: Estado do botão de pausar
+         * \param[in]       state: Estado atual do jogo
+         * \param[out]      state: Novo estado do jogo após a ação
+         * \param[out]      pause_time: Tempo de início da pausa
          */
         if (button1 && state == RUNNING) {
             state = PAUSED;
@@ -186,6 +196,10 @@ void* movimentoToupeira(void* arg) {
          * 
          * Quando o botão de pausar (button1) é pressionado novamente e o estado atual é PAUSED, 
          * a função define o estado para RUNNING, remove os blocos de pausa e ajusta o tempo total de pausa.
+         * \param[in]       button1: Estado do botão de pausar
+         * \param[in]       state: Estado atual do jogo
+         * \param[out]      state: Novo estado do jogo após a ação
+         * \param[out]      total_pause_time: Tempo total de pausa ajustado
          */
         if (button1 && state == PAUSED) {
             state = RUNNING;
@@ -199,6 +213,13 @@ void* movimentoToupeira(void* arg) {
          * Quando o botão de reiniciar (button2) é pressionado, a função redefine o endereço do bloco base, 
          * define o estado para RUNNING, reinicia a pontuação, o tempo total de pausa, o tempo de início e o 
          * último tempo de verificação. Em seguida, redesenha a tela do jogo.
+         * \param[in]       button2: Estado do botão de reiniciar
+         * \param[out]      base_block_address: Novo endereço do bloco base
+         * \param[out]      state: Novo estado do jogo após a ação
+         * \param[out]      pontuacao: Pontuação reiniciada
+         * \param[out]      total_pause_time: Tempo total de pausa reiniciado
+         * \param[out]      start_time: Tempo de início reiniciado
+         * \param[out]      last_check_time: Último tempo de verificação reiniciado
          */
         if (button2) {
             base_block_address = 315;
@@ -215,6 +236,8 @@ void* movimentoToupeira(void* arg) {
          * 
          * Quando o botão de parar (button3) é pressionado em qualquer estado, 
          * a função define o estado para ENDED_BY_BUTTON, limpa os sprites e o bloco de fundo, e sai do loop.
+         * \param[in]       button3: Estado do botão de parar
+         * \param[out]      state: Novo estado do jogo após a ação
          */
         if (button3) {
             state = ENDED_BY_BUTTON;
@@ -237,6 +260,9 @@ void* movimentoToupeira(void* arg) {
              * limite, invertendo a direção ou parando o movimento conforme necessário.
              * Se a toupeira não estiver em movimento, verifica se é hora de retomar o movimento 
              * com base no intervalo definido.
+             * \param[in]       toupeiras: Array de toupeiras
+             * \param[in]       current_time: Tempo atual
+             * \param[out]      toupeiras[i]: Posição e estado atualizado da toupeira
              */
             for (i = 0; i < 9; i++) {
                 if (toupeiras[i]->moving) {
@@ -270,7 +296,15 @@ void* movimentoToupeira(void* arg) {
             }
 
 
-        
+            /**
+             * \brief Atualiza o bloco de fundo periodicamente
+             * 
+             * A cada 5 segundos, a função atualiza o bloco de fundo e ajusta o endereço do bloco base.
+             * \param[in]       current_time: Tempo atual
+             * \param[in]       last_check_time: Último tempo de verificação
+             * \param[out]      base_block_address: Novo endereço do bloco base
+             * \param[out]      last_check_time: Tempo atualizado da última verificação
+             */
             if ((current_time - last_check_time) >= 5) {
                 set_background_block(base_block_address, 1, 4, 6);
                 base_block_address -= 1;
@@ -278,6 +312,17 @@ void* movimentoToupeira(void* arg) {
             } /*cada 5 seg*/
 
           
+            /**
+             * \brief Encerra o jogo após 60 segundos de execução efetiva
+             * 
+             * Quando o tempo total de jogo, descontando o tempo de pausa, atinge 60 segundos, 
+             * a função define o estado para ENDED_BY_TIME, limpa os sprites e o bloco de fundo, e 
+             * desenha a tela de "Game Over".
+             * \param[in]       current_time: Tempo atual
+             * \param[in]       start_time: Tempo de início do jogo
+             * \param[in]       total_pause_time: Tempo total de pausa
+             * \param[out]      state: Novo estado do jogo após a ação
+             */
             if ((current_time - start_time - total_pause_time) >= 60) {
                 state = ENDED_BY_TIME;
                 clear_sprite();
@@ -331,7 +376,18 @@ void* mouse(void* arg) {
             change_coordinate(martelo, x, y);
 
 
-            // Verifica condições para pontuar
+            /**
+             * \brief Verifica colisões e atualiza a pontuação
+             * 
+             * A função verifica se o martelo colidiu com alguma toupeira enquanto o botão esquerdo 
+             * do mouse está pressionado e a toupeira está dentro dos limites. Se houver colisão, 
+             * a pontuação é incrementada.
+             * \param[in]       toupeiras: Array de toupeiras
+             * \param[in]       martelo: Sprite do martelo
+             * \param[in]       leftButton: Estado do botão esquerdo do mouse
+             * \param[in]       state: Estado atual do jogo
+             * \param[out]      pontuacao: Pontuação atualizada
+             */
             int i;
             for (i = 0; i < 9; i++) {
                  if (collision(toupeiras[i], martelo) && leftButton && toupeiras[i]->coord_y <= toupeiras[i]->max_y && state == RUNNING) {
@@ -362,6 +418,13 @@ void* mouse(void* arg) {
 /* Escreve sprites na memória a partir do offset 25 */
 void write_sprites() {
 
+    /**
+     * \brief Escreve os sprites do martelo na memória
+     * 
+     * A função escreve os dados dos sprites do martelo na memória, começando no endereço 10000.
+     * \param[in]       marteloSp: Dados dos RGB sprites do martelo
+     * \param[out]      endereco_memoria: Endereço na memória onde os dados são escritos
+     */
     int i;
     for (i = 0; i < 380; i++) {
         int R = marteloSp[i][0];
@@ -371,6 +434,13 @@ void write_sprites() {
         write_sprite_mem(R, G, B, endereco_memoria);
     }
 
+    /**
+     * \brief Escreve os sprites das toupeiras na memória
+     * 
+     * A função escreve os dados dos sprites das toupeiras na memória, começando no endereço 10400.
+     * \param[in]       toupeiraSp: Dados dos RGB sprites das toupeiras
+     * \param[out]      endereco_memoria: Endereço na memória onde os dados são escritos
+     */
     for (i = 0; i < 400; i++) {
         int R = toupeiraSp[i][0];
         int G = toupeiraSp[i][1];
@@ -379,6 +449,13 @@ void write_sprites() {
         write_sprite_mem(R, G, B, endereco_memoria);
     }
 
+    /**
+     * \brief Escreve os sprites dos arbustos na memória
+     * 
+     * A função escreve os dados dos sprites dos arbustos na memória, começando no endereço 10800.
+     * \param[in]       arbustoSp: Dados dos RGB sprites dos arbustos
+     * \param[out]      endereco_memoria: Endereço na memória onde os dados são escritos
+     */
     for (i = 0; i < 400; i++) {
         int R = arbustoSp[i][0];
         int G = arbustoSp[i][1];
@@ -407,6 +484,14 @@ int main() {
 
 
 /* ============ CRIAÇÃO DE SPRITES ============= */
+
+
+    /**
+     * \brief Inicializa as toupeiras
+     * 
+     * Cria e configura as instâncias das toupeiras para a primeira linha com as propriedades
+     * de coordenadas, offset, data register e limites de movimento.
+     */
 
     /* Toupeiras - linha 1*/
     Sprite toupeira1;
@@ -442,6 +527,13 @@ int main() {
     toupeira3.min_y = 300;
     toupeira3.max_y = 285;
 
+    /**
+     * \brief Inicializa as toupeiras para a segunda linha
+     * 
+     * Cria e configura as instâncias das toupeiras para a segunda linha com as propriedades
+     * de coordenadas, offset, data register e limites de movimento.
+     */
+
     /* Toupeiras - linha 2 */
     Sprite toupeira4;
     toupeira4.coord_x = 200;
@@ -476,6 +568,13 @@ int main() {
     toupeira6.min_y = 350;
     toupeira6.max_y = 335;
 
+
+    /**
+     * \brief Inicializa as toupeiras para a terceira linha
+     * 
+     * Cria e configura as instâncias das toupeiras para a terceira linha com as propriedades
+     * de coordenadas, offset, data register e limites de movimento.
+     */
     /* Toupeiras - linha 3 */
     Sprite toupeira7;
     toupeira7.coord_x = 200;
@@ -510,6 +609,12 @@ int main() {
     toupeira9.min_y = 400;
     toupeira9.max_y = 385;
 
+    /**
+     * \brief Inicializa as toupeiras e as armazena em um array
+     * 
+     * Cria um array de ponteiros para as instâncias das toupeiras, permitindo acesso
+     * para manipulação durante a execução do jogo.
+     */
     Sprite* toupeiras[9] = {
         &toupeira1,
         &toupeira2,
@@ -522,6 +627,12 @@ int main() {
         &toupeira9
     };
 
+    /**
+     * \brief Inicializa o martelo
+     * 
+     * Cria e configura a instância do martelo com as propriedades de coordenadas, offset,
+     * data register e estado de atividade.
+     */
     Sprite_Fixed martelo;
     martelo.coord_x = 0;
     martelo.coord_y = 0;
@@ -530,7 +641,13 @@ int main() {
     martelo.data_register = 1;
 
 
-    /* Arbustos - linha 1 */
+    /**
+     * \brief Inicializa os arbustos
+     * 
+     * Cria e configura as instâncias dos arbustos para cada linha com as propriedades
+     * de coordenadas, offset, data register e estado de atividade.
+     */
+        /* Arbustos - linha 1 */
     Sprite_Fixed arbusto1;
     arbusto1.coord_x = 200;
     arbusto1.coord_y = 300;
@@ -553,6 +670,12 @@ int main() {
     arbusto3.data_register = 4; 
 
 
+    /**
+     * \brief Inicializa os arbustos para a segunda linha
+     * 
+     * Cria e configura as instâncias dos arbustos para a segunda linha com as propriedades
+     * de coordenadas, offset, data register e estado de atividade.
+     */
     /* Arbustos - linha 2 */
     Sprite_Fixed arbusto4;
     arbusto4.coord_x = 200;
@@ -575,6 +698,12 @@ int main() {
     arbusto6.ativo = 1;
     arbusto6.data_register = 7; 
 
+    /**
+     * \brief Inicializa os arbustos para a terceira linha
+     * 
+     * Cria e configura as instâncias dos arbustos para a terceira linha com as propriedades
+     * de coordenadas, offset, data register e estado de atividade.
+     */
     /* Arbustos - linha 3 */
     Sprite_Fixed arbusto7;
     arbusto7.coord_x = 200;
@@ -597,6 +726,12 @@ int main() {
     arbusto9.ativo = 1;
     arbusto9.data_register = 10; 
 
+    /**
+     * \brief Inicializa os arbustos e os armazena em um array
+     * 
+     * Cria um array de ponteiros para as instâncias dos arbustos, permitindo acesso
+     * fácil para manipulação durante a execução do jogo.
+     */
     Sprite_Fixed* arbustos[9] = {
         &arbusto1,
         &arbusto2,
@@ -609,11 +744,22 @@ int main() {
         &arbusto9
     };
 
+    /**
+     * \brief Prepara os argumentos para as threads
+     * 
+     * Cria um array de ponteiros para passar as referências do martelo, das toupeiras e dos arbustos
+     * para as threads que serão criadas.
+     */
     void* args[3] = { &martelo, &toupeiras, &arbustos};
 
   
     /*============== CRIAÇÃO DE THREADS ================== */
-
+    /**
+     * \brief Cria e inicia as threads para movimento das toupeiras e controle do mouse
+     * 
+     * Cria duas threads: uma para movimentar as toupeiras e outra para o controle do mouse.
+     * As threads são configuradas e os erros de criação e junção são tratados.
+     */
     if (pthread_create(&thread1, NULL, movimentoToupeira, (void*)args) != 0) {
         perror("Failed to create thread 1");
         return 1;
